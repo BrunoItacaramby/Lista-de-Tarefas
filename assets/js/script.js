48f8605b4b08da713fafc5f81a1da738257ac682
BrunoItacaramby <brunobob69@hotmail.com>
2024-03-25 23:16:28 -0300

    Copiando o projeto Lista de Tarefas para um repositorio proprio

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+inputTarefa = document.querySelector('.input-tarefa')
+btnTarefa = document.querySelector('.btn-tarefa')
+tarefas = document.querySelector('.tarefas')
+
+function criaLi() {
+    const li = document.createElement('li')
+    return li
+}
+
+function limpaInput () {
+    inputTarefa.value = ''
+    inputTarefa.focus()
+}
+
+function criaTarefa(inputTexto){
+    const li = criaLi()
+    li.innerText = inputTexto
+    tarefas.appendChild(li)
+    criaBotaoApagar(li)
+    limpaInput()
+    salvarTarefas()
+}
+
+function criaBotaoApagar(li) {
+    const botaoApagar = document.createElement('button')
+    botaoApagar.innerText = 'Apagar'
+    botaoApagar.setAttribute('class', 'apagar')
+    botaoApagar.setAttribute('title', 'apagar essa tarefa')
+    li.appendChild(botaoApagar)
+}
+
+document.addEventListener('click', function(event){
+    const element = event.target
+    if(element.classList.contains('apagar')){
+        element.parentElement.remove()
+        salvarTarefas()
+    }
+})
+
+inputTarefa.addEventListener('keypress', function(event){
+    if(event.keyCode === 13){
+        if(!inputTarefa.value) return3
+        criaTarefa(inputTarefa.value)
+    }
+})
+
+btnTarefa.addEventListener('click', function(){
+    if(!inputTarefa.value) return
+    criaTarefa(inputTarefa.value)
+})
+
+function salvarTarefas() {
+    const tarefasLi = tarefas.querySelectorAll('li')
+    let listaDeTarefas = []
+    
+
+    for (const tarefa of tarefasLi) {
+        let tarefaTexto = tarefa.innerText
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto)           
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas)
+    localStorage.setItem('tarefas', tarefasJSON)
+}
+
+function criaTarefasSalvas() {
+    const tarefasSalvas = localStorage.getItem('tarefas')
+    const tarefasSalvasJSON = JSON.parse(tarefasSalvas)
+
+    for (const tarefa of tarefasSalvasJSON) {
+        criaTarefa(tarefa)
+    }
+}
+
+criaTarefasSalvas()
